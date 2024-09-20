ca7f4f3e0c547f3a3c036b55a4ccd3bfa2352490
andy26666 <7009221@spsd.sk.ca>
2024-09-20 10:33:36 -0600

    Interactive Scene

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,11 +6,11 @@ let Img;
 let GameImg;
 let GamestageImg;
 let CatImg;
-let DodgeBallImg;
 let Startbutton;
 let x = 200;
 let y = 200;
 let speed = 5;
+let ColorList = ['white','red','navy','purple', 'orange', 'blue','black','pink','green'];
 let timer = 3;
 
 //variable of scene checker
@@ -19,6 +19,7 @@ var description = false;
 var playscene = false;
 var IsGame = false;
 var OverWall = false;
+var GameOver = false;
 
 function setup() {
   
@@ -27,7 +28,6 @@ function setup() {
   GameImg = loadImage('Game.jpg');
   GamestageImg = loadImage('Playscene.png');
   CatImg = loadImage('Cat.png');
-  DodgeBallImg = loadImage('DodgeBall.png');
   createCanvas(700, 400);
   
   GameButton();
@@ -77,7 +77,7 @@ function GameText() {
     fill('skyblue');
 
     textSize(40);
-    text('DODGE BALL',400-textWidth(),100);
+    text('COlOUR GAME',400-textWidth(),100);
   
   // creater text
     textSize(25);
@@ -97,8 +97,8 @@ function GameText() {
     textSize(25);
     textAlign(CENTER);
     textWrap(WORD);
-    text('Press WASD to move. Press R to rush, E to catch.', 350,150);
-    text('Press Tab to pause.',350, 200);
+    text('Press WASD to move.', 350,150);
+    text('Press Q to pause.',350, 200);
     text('Press Any key to start', 350, 300);
   }
   if (playscene === true) {
@@ -146,6 +146,7 @@ function keyPressed() {
 
 function GameSet() {
   You();
+  ColourScreen();
 }
 
 //function of your character
@@ -179,4 +180,17 @@ function IsWall() {
   if (y >= height - CatImg.height*0.2 || y <= 0 + CatImg.height*0.2) {
     OverWall = true;
   }
+}
+
+function ColourScreen() {
+  square(width/2 - 25, 25, 75);
+  fill(random(ColorList));
+  if (frameCount % 60 === 0 && timer > 0) { 
+      timer -= 1;    
+     }
+  else if (timer === 0) {
+    
+  }
+  
+
 }

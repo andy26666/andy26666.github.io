3288aa861b3c4b9d5a74d5dd8963fdd1c67e87f2
andy26666 <7009221@spsd.sk.ca>
2024-09-25 11:01:47 -0600

    Update sketch.js

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -7,7 +7,7 @@ let GameImg;
 let GamestageImg;
 let CatImg;
 let Startbutton;
-let ColorList = ['white','red','navy','purple', 'orange', 'blue','black','pink','green', 'lightblue', 'yellow', 'darkblue', 'lightgreen', 'darkgreen', 'grey'];
+let ColorList = ["white","red","navy","purple", "orange", "blue","black","pink","green", "lightblue", "yellow", "darkblue", "lightgreen", "darkgreen", "grey"];
 let ColorChoice;
 let ColorScreenChoice;
 let CorrectPlace;
@@ -18,28 +18,27 @@ let x = 200;
 let y = 50;
 
 //variable of scene checker
-var buttonTrue = true;
-var description = false;
-var playscene = false;
-var IsGame = false;
-var IsPause = false;
+let buttonTrue = true;
+let description = false;
+let playscene = false;
+let IsGame = false;
+let IsPause = false;
 
 //variable for wall
-var FrontWall = false;
-var BehindWall = false;
-var LeftWall = false;
-var RightWall = false;
+let FrontWall = false;
+let BehindWall = false;
+let LeftWall = false;
+let RightWall = false;
 
-
-var GameOver = false;
+let GameOver = false;
 
 function setup() {
   
   //img of game
-  Img = loadImage('Start Scene.png');
-  GameImg = loadImage('Game.jpg');
-  GamestageImg = loadImage('Playscene.png');
-  CatImg = loadImage('Cat.png');
+  Img = loadImage("Start Scene.png");
+  GameImg = loadImage("Game.jpg");
+  GamestageImg = loadImage("Playscene.png");
+  CatImg = loadImage("Cat.png");
   createCanvas(700, 400);
   
   ColorChoice = random(ColorList);
@@ -61,10 +60,10 @@ function draw() {
     GameText();  
     Startbutton.show();
   }
-  if (description == true) {
-      background(GameImg);
-      Startbutton.hide();
-      GameText();
+  if (description === true) {
+    background(GameImg);
+    Startbutton.hide();
+    GameText();
   }
   if (playscene === true) {
     background(GamestageImg);
@@ -78,9 +77,9 @@ function draw() {
 }
 
 function GameButton() {
-    // start button
+  // start button
   if (buttonTrue === true){
-    Startbutton = createButton('START');
+    Startbutton = createButton("START");
     Startbutton.size(150,50);
     Startbutton.position(275, 300);
     
@@ -89,63 +88,62 @@ function GameButton() {
 }
 
 function GameText() {
-    //Title text of start scene
+  //Title text of start scene
   
   if (buttonTrue === true) {
-    fill('skyblue');
+    fill("skyblue");
 
     textSize(40);
-    text('COlOUR GAME',400-textWidth(),100);
- 
+    text("COlOUR GAME",400-textWidth(),100);
   }
-  
+ 
   //text of description
   if (description === true) {
     
     //title of introduction
-    fill('black');
+    fill("black");
     textSize(40);
     textAlign(CENTER);
-    text('HOW TO PLAY?',350, 50);
+    text("HOW TO PLAY?",350, 50);
     
     //text of small sentence
     textSize(25);
     textAlign(CENTER);
-    textWrap(WORD);
-    text('Rule', 350, 100);
-    text('Go to the same colour square within 3 seconds.', 350, 125);
-    text('Control', 350,200);
-    text('Press WASD to move.', 350,225);
-    text('Press Q to pause.',350, 250);
-    text('Press Any key to start', 350, 300);
+    text("Rule", 350, 100);
+    text("Go to the same colour square within 3 seconds.", 350, 125);
+    text("Control", 350,200);
+    text("Press WASD to move.", 350,225);
+    text("Press Q to pause.",350, 250);
+    text("Press Any key to start", 350, 300);
     
     textSize(15);
-    text('created by Kevin Lee', 600,350);
+    text("created by Kevin Lee", 600,350);
   }
   if (playscene === true) {
     textSize(80);
     textAlign(CENTER, CENTER);
     text(timer,width/2, height/2);
     time();
+  }
 
   if (IsPause === true) {
-    fill('pink');
+    fill("pink");
     rect(100,100, 500, 200);
-    fill('black');
+    fill("black");
     textSize(30);
-    text('Pause, Click P to continue!', 350, 200);
-  }
+    text("Pause, Click P to continue!", 350, 200);
   }
 }
 
+
 function time() {
-    if (frameCount % 60 === 0 && timer > 0) { 
-      timer -= 1;    
-     }
-    else if (timer === 0) {
-      playscene = false;
-      IsGame = true;
-    }
+  if (frameCount % 60 === 0 && timer > 0) { 
+    timer -= 1;    
+  }
+  else if (timer === 0) {
+    playscene = false;
+    IsGame = true;
+  }
 }
 
 //function of the game
@@ -163,6 +161,13 @@ function keyPressed() {
     description = false;
     playscene = true;
   }
+  if (IsGame === true && keyIsDown(80)) {
+    IsPause = true; 
+    GameText();
+  }
+  if (IsPause === true && keyIsDown(80)) {
+    IsPause = false;
+  }
 }
 /////////////////////////////////////////
 //function of game
@@ -174,48 +179,46 @@ function GameSet() {
 
 //function of your character
 function You() {
-  image(CatImg, x, y,
-        CatImg.width*0.3, CatImg.height*0.2);
+  image(CatImg, x, y, CatImg.width*0.3, CatImg.height*0.2);
   KeyInteract();
   IsWall();
 }
 
 function KeyInteract() {
   
-  if (FrontWall === false) {
-    if (keyIsDown(87)) {  //w
-    y -= speed;
-  }
-  }
-  
-  if (BehindWall === false) {
-    if (keyIsDown(83)) {  //s
-    y += speed;
-  }
-  }
-  
-  if (LeftWall === false) {
-    if (keyIsDown(65)) {  //a
-    x -= speed;
-  }
-  }
-  
-  if (RightWall === false) {
-    if (keyIsDown(68)) {  //d
-    x += speed;
-  }
-  }
-  if (key === 'p') {
+  if (IsPause === false) {
+    if (FrontWall === false) {
+      if (keyIsDown(87)) {  //w
+        y -= speed;
+      }
+    }
     
+    if (BehindWall === false) {
+      if (keyIsDown(83)) {  //s
+        y += speed;
+      }
+    }
+    
+    if (LeftWall === false) {
+      if (keyIsDown(65)) {  //a
+        x -= speed;
+      }
+    }
+    
+    if (RightWall === false) {
+      if (keyIsDown(68)) {  //d
+        x += speed;
+      }
+    }
   }
-  
 }
 
 function IsWall() {
   // if wall on left
   if (x <= -40 ) {
     LeftWall = true;
-  }else {
+  }
+  else {
     LeftWall = false;
   }
 
@@ -223,21 +226,24 @@ function IsWall() {
   //wall on right
   if (x >= 600) {
     RightWall = true;
-  }else {
+  }
+  else {
     RightWall = false;
   }
   
   //wall on behind
   if (y >= 335) {
     BehindWall = true;
-  } else {
+  } 
+  else {
     BehindWall = false;
   }
   
   //wall on front
   if (y <= -20) {
     FrontWall = true;
-  } else {
+  } 
+  else {
     FrontWall = false;
   }
 }
@@ -259,9 +265,9 @@ function ColorGround() {
 }
 
 function Pause() {
-  fill('pink');
+  fill("pink");
   let pause_bar = rect(100,100, 500, 200);
-  fill('black');
+  fill("black");
   textSize(30);
-  text('Pause, Click P to continue!', 350, 200);
+  text("Pause, Click P to continue!", 350, 200);
 }

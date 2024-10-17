cf9d58664a2ac79087dcd9d1c1eaeb1368af9ce5
andy26666 <7009221@spsd.sk.ca>
2024-10-17 10:26:38 -0600

    Update sketch.js

--- array-json/sketch.js
+++ array-json/sketch.js
@@ -11,6 +11,7 @@ let ball;
 let bat;
 let position;
 let choice;
+let attack;
 
 let directOfPlayer;
 let PLAYER_X;
@@ -19,8 +20,16 @@ let new_y;
 let bat_wid;
 let bat_hei;
 
-let playerPosit;
+let isAttack = false;
+let normalAtt = false;
+let playerPosit_x;
+let playerPosit_y;
 let n = 0;
+let k = 0;
+let dy = 0;
+
+let c1 = "white";
+let c2 = "white";
 
 function preload() {
   Img = loadImage("startScene.jpg");
@@ -40,14 +49,13 @@ function setup() {
 function draw() {
 
   background(Img);
-
-
-  playerSet();
+  
+  circleBar();
   ballSet();
-  gameOver();
+  playerSet();
+  //gameOver();
 
 }
-
 
 function ballSet() {
   ball = image(ballImg, position.x, position.y, 40, 40);
@@ -91,41 +99,96 @@ function posit() {
   return position;
 }
 function keyTyped() {
-  if (n > -1 && n < 4) {
+  
+  // for playerPosit_x, n can't be lesser or greater than the array
+  if (n > 0) {
     if (key === 'a') {
-      console.log(n -= 1);
+      n -= 1;
+      }
     }
+  if (n < 3) {
     if (key === 'd') {
-      console.log(n += 1);
+      n += 1;
     }
   }
-  else {
-    if (n === 4) {
-      n = 3;
-    }
-    if (n === -1) {
-      n = 0;
+  
+  // for playerPosit_y, dy can't be lesser or greater than the arra
+  
+  //attack set
+  if (isAttack) {
+    if (key === 'w') {
+      // 1 is normal attack, 2 is special attack
+      attack = [1,1,2];
+      if (attack[k] === 1) {
+        if (dy === 0) {
+          dy += 1;
+          normalAtt = true;
+        }
+      }
+      else {
+        console.log("special attack");
+        k = 0;
+      }
     }
   }
 }
 
 function playerSet() {
+  isAttack = true;
+
+  // player movesetting
   PLAYER_X = width/8-playerImg.width*0.02;
   PLAYER_Y = height-playerImg.height*0.07;
-  playerPosit = [PLAYER_X, 4.3*PLAYER_X, 7.6*PLAYER_X,11*PLAYER_X];
+  playerPosit_x = [PLAYER_X, 4.3*PLAYER_X ,7.6*PLAYER_X,11*PLAYER_X];
+  
+  playerPosit_y = [PLAYER_Y, PLAYER_Y-30];
 
+  playerPosit_x[n] = 0;
+  playerPosit_y[dy] = 0;
+  translate(playerImg.width*0.08, playerImg.height*0.07);
+  rotate(45);
 
-  image(playerImg, playerPosit[n], PLAYER_Y, playerImg.width*0.08, playerImg.height*0.07);
-  console.log(n);
+  //display player
+  image(playerImg, playerPosit_x[n], playerPosit_y[dy], playerImg.width*0.08, playerImg.height*0.07);
+  
+  if (normalAtt) {
+    k += 1;
+    dy -= 1;
+    normalAtt = false;
+  }
+
+  
+
+}
+
+function circleBar() {
+  fill(c1);
+  circle(width-70,height-50,10);
+  fill(c2);
+  circle(width-50,height-50, 15);
+  
+  if (k === 0) {
+    c1 = "white";
+    c2 = "white";
+  }
+  if (k === 1) {
+    c1 = "red";
+  }
+  if (k === 2) {
+    c2 = "red";
+    console.log("special attack is ready");
+
+  }
+  
 }
 
 
 function gameOver() {
   // detect the collision for the ball and the bat
-  d = (playerPosit[n], PLAYER_Y, position.x, position.y);
+  d = (playerPosit_x[n], playerPosit_y[dy], position.x, position.y);
   // if collide, game over
-  if (d < 30) {
-    console.log("game over");
+  if (d === 0) {
+    overScene();
   }
 }
   
@@ -137,8 +200,5 @@ function overScene() {
   text("GAME OVER", width/2-30, height/2-20);
 }
 
-function shoot() {
-  let attack = {
-    
-  };
-}
+
+

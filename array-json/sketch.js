5fd16b117dd4d331bb6675159a13b49ecc45e87c
andy26666 <7009221@spsd.sk.ca>
2024-10-18 11:03:03 -0600

    array-json
    2024/10/18
    I try to create the attack of gun.
    And I make the life of the player and death when all of them are remove.

--- array-json/sketch.js
+++ array-json/sketch.js
@@ -5,7 +5,7 @@
 let ballImg;
 let playerImg;
 let Img;
-let directImg;
+let gunImg;
 
 let isLose = false;
 let ball;
@@ -20,15 +20,22 @@ let PLAYER_Y;
 let new_y;
 let bat_wid;
 let bat_hei;
+let to = 60;
 
 let isAttack = false;
 let normalAtt = false;
+let removeHeart = false;
+let isGun = false;
 let playerPosit_x;
 let playerPosit_y;
 let n = 0;
 let k = 0;
 let dy = 0;
 
+let button;
+let cc = [];
+let gunBullet = [1,1,2,0];
+let gb = 0;
 let c1 = "white";
 let c2 = "white";
 
@@ -36,31 +43,43 @@ function preload() {
   Img = loadImage("startScene.jpg");
   playerImg = loadImage("bat.png");
   ballImg = loadImage("ball.gif");
-  directImg = loadImage("direction.png");
+  gunImg = loadImage("gun.png");
 
 }
 
 function setup() {
   createCanvas(400, 400);
+
   posit();
+  preload();
 }
 
+
 function draw() {
-
   background(Img);
-  
-  circleBar();
-  ballSet();
+  if (removeHeart === true) {
+    to-=20;
+    removeHeart = false;
+    if (to <= 0) {
+      gameOver();
+      isLose = true;
+    }
+  }
+  for (let r = 20; r <= to; r+= 20) {
+    heart(15, r, 15);
+  }
   playerSet();
+  ballSet();
   //gameOver();
-
 }
 
 function ballSet() {
-  ball = image(ballImg, position.x, position.y, 40, 40);
+  if (!isLose) {
+    ball = image(ballImg, position.x, position.y, 40, 40);
+  }
 
-  if (isLose !== true) { 
-    position.y += 2;
+  if (!isLose) { 
+    position.y += 5;
     if (position.y > height) {
       posit();
     }
@@ -103,8 +122,8 @@ function keyTyped() {
   if (n > 0) {
     if (key === 'a') {
       n -= 1;
+      }
     }
-  }
   if (n < 3) {
     if (key === 'd') {
       n += 1;
@@ -124,17 +143,16 @@ function keyTyped() {
           normalAtt = true;
         }
       }
-      else {
-        console.log("special attack");
-        k = 0;
+      if (attack[k] === 2) {
+
+      }
+
       }
     }
   }
-}
 
 function playerSet() {
   isAttack = true;
-
   // player movesetting
   PLAYER_X = width/8-playerImg.width*0.02;
   PLAYER_Y = height-playerImg.height*0.07;
@@ -143,7 +161,9 @@ function playerSet() {
   playerPosit_y = [PLAYER_Y, PLAYER_Y-30];
 
   //display player
-  image(playerImg, playerPosit_x[n], playerPosit_y[dy], playerImg.width*0.08, playerImg.height*0.07);
+  if (!isLose) {
+    image(playerImg, playerPosit_x[n], playerPosit_y[dy], playerImg.width*0.08, playerImg.height*0.07);
+  }
   
   if (normalAtt) {
     k += 1;
@@ -151,48 +171,88 @@ function playerSet() {
     normalAtt = false;
   }
   if (k === 2) {
-    specialAtt(playerPosit_x[n], playerPosit_y[dy]);
+    gunIcon();
   }
+
 }
 
-function circleBar() {
-  fill(c1);
-  circle(width-70,height-50,10);
-  fill(c2);
-  circle(width-50,height-50, 15);
+function circleBar(x,y,size) {
   
   if (k === 0) {
-    c1 = "white";
-    c2 = "white";
+    cc.push(y);
+    console.log(cc[0]);
   }
   if (k === 1) {
-    c1 = "red";
+    
   }
   if (k === 2) {
     c2 = "red";
     console.log("special attack is ready");
+
   }
 }
 
-function specialAtt() {
-  image(directImg, playerPosit_x[n] + 50, playerPosit_y[dy], directImg.width * 0.05, directImg.height * 0.05);
+function heart(x, y, size) {
+  beginShape();
+  //set x,y position
+  vertex(x, y);
+  fill("red");
+  //draw left side of heart
+  bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+  //draw right side of heart
+  bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+  endShape(CLOSE);
+}
+
+function gunIcon() {
+  image(gunImg, 5,90, gunImg.width*0.03, gunImg.height*0.03);
+  
+}
+
+function attackGun() {
+
+
+  if (gunBullet[gb] === 1) {
+    square(30,30,20,20)
+    gb += 1;
+  }
+  if (gunBullet[gb] === 2) {
+    square(30,30,40,40);
+    gb += 1;
+  }
+  else {
+    k = 0;
+  }
+  console.log("gun");
 }
 
 function gameOver() {
-  // detect the collision for the ball and the bat
-  d = (playerPosit_x[n], playerPosit_y[dy], position.x, position.y);
-  // if collide, game over
-  if (d === 0) {
-    overScene();
+  // detect the collision for the ball and the line
+
+  if (!isLose) {
+    d = (0, height, position.x, position.y);
+    // if collide, game over
+    if (d === 0) {
+      removeHeart = true;
+    }
+  }
+
+  if (isLose) {
+    background("black");
+    fill("white");
+    text("GAME OVER", width/2-30, height/2-20);
+    text("Click anywhere to start", width/2-50, height/2+30);
   }
 }
-  
-function overScene() {
-  isLose = true;
-  background("black");
-  fill("white");
-  text("GAME OVER", width/2-30, height/2-20);
+
+function mouseClicked() {
+  if (isLose) {
+    to = 60;
+    isLose = false;
+  }
 }
+
+
 
 
 

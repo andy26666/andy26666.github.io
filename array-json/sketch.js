df2f24837bd4e66a9fc383d12ff6f68a13a02dea
andy26666 <7009221@spsd.sk.ca>
2024-10-21 11:02:59 -0600

    Update sketch.js
    2024/10/21, I make the change of gun and bat. I create the bat and gun attack.

--- array-json/sketch.js
+++ array-json/sketch.js
@@ -34,10 +34,7 @@ let dy = 0;
 
 let button;
 let cc = [];
-let gunBullet = [1,1,2,0];
 let gb = 0;
-let c1 = "white";
-let c2 = "white";
 
 function preload() {
   Img = loadImage("startScene.jpg");
@@ -57,6 +54,7 @@ function setup() {
 
 function draw() {
   background(Img);
+  // remove heart when ball hit screen
   if (removeHeart === true) {
     to-=20;
     removeHeart = false;
@@ -70,9 +68,10 @@ function draw() {
   }
   playerSet();
   ballSet();
+  weaponIcon();
   //gameOver();
 }
-
+// ball display and ball movevment speed
 function ballSet() {
   if (!isLose) {
     ball = image(ballImg, position.x, position.y, 40, 40);
@@ -89,25 +88,28 @@ function ballSet() {
 // spawn ball in random place
 function posit() {
   choice = random(100);
-  
+  //case 1
   if (choice <= 25) {
     position = {
       x: width/8,
       y: 0,
     };
   }
+  //case 2
   else if (choice <= 50) {
     position = {
       x: 3*width/8,
       y: 0,
     };
   }
+  //case 3
   else if (choice <= 75) {
     position = {
       x: 5*width/8,
       y: 0,
     };
   }
+  //case 4
   else if (choice <= 100) {
     position = {
       x: 7*width/8,
@@ -122,8 +124,8 @@ function keyTyped() {
   if (n > 0) {
     if (key === 'a') {
       n -= 1;
-      }
     }
+  }
   if (n < 3) {
     if (key === 'd') {
       n += 1;
@@ -131,26 +133,43 @@ function keyTyped() {
   }
   
   // for playerPosit_y, dy can't be lesser or greater than the arra
-  
+  let id = height-50;
   //attack set
   if (isAttack) {
     if (key === 'w') {
-      // 1 is normal attack, 2 is special attack
-      attack = [1,1,2];
-      if (attack[k] === 1) {
-        if (dy === 0) {
-          dy += 1;
-          normalAtt = true;
+      // 1 is bat attack, 2 is change to gun
+      if (isGun === false) {
+        attack = [1,1,2];
+        if (attack[k] === 1) {
+          if (dy === 0) {
+            dy += 1;
+            normalAtt = true;
+          }
+        }
+        else {
+          isGun = true;
+          k = 0;
         }
       }
-      if (attack[k] === 2) {
-
+      // 1 is gun attack, 2 is change to bat
+      else {
+        attack = [1,1,1,2];
+        if (attack[k] === 1) {
+ 
+          circle(30, id,30);
+          k += 1;
+          id -= 7;
+        }
+        else {
+          k = 0;
+          isGun = false;
+        }
       }
+    } 
+  } 
+}
 
-      }
-    }
-  }
-
+// player set of bat
 function playerSet() {
   isAttack = true;
   // player movesetting
@@ -161,34 +180,17 @@ function playerSet() {
   playerPosit_y = [PLAYER_Y, PLAYER_Y-30];
 
   //display player
-  if (!isLose) {
+  if (!isLose && isGun === false) {
     image(playerImg, playerPosit_x[n], playerPosit_y[dy], playerImg.width*0.08, playerImg.height*0.07);
   }
   
   if (normalAtt) {
-    k += 1;
-    dy -= 1;
     normalAtt = false;
+    dy -= 1;
+    k += 1;
   }
-  if (k === 2) {
-    gunIcon();
-  }
-
-}
-
-function circleBar(x,y,size) {
-  
-  if (k === 0) {
-    cc.push(y);
-    console.log(cc[0]);
-  }
-  if (k === 1) {
-    
-  }
-  if (k === 2) {
-    c2 = "red";
-    console.log("special attack is ready");
-
+  if (isGun) {
+    attackGun(playerPosit_x, playerPosit_y, n, dy);
   }
 }
 
@@ -204,26 +206,18 @@ function heart(x, y, size) {
   endShape(CLOSE);
 }
 
-function gunIcon() {
-  image(gunImg, 5,90, gunImg.width*0.03, gunImg.height*0.03);
-  
+//display the weapon that I hold for icon
+function weaponIcon() {
+  if (isGun) {
+    image(gunImg, 5,90, gunImg.width*0.03, gunImg.height*0.03);
+  } 
+  else {
+    image(playerImg,-20, 90, playerImg.width*0.07, playerImg.height*0.04);
+  }
 }
 
 function attackGun() {
-
-
-  if (gunBullet[gb] === 1) {
-    square(30,30,20,20)
-    gb += 1;
-  }
-  if (gunBullet[gb] === 2) {
-    square(30,30,40,40);
-    gb += 1;
-  }
-  else {
-    k = 0;
-  }
-  console.log("gun");
+  image(gunImg, playerPosit_x[n], playerPosit_y[dy], gunImg.width*0.05, gunImg.height*0.05);
 }
 
 function gameOver() {
@@ -231,12 +225,12 @@ function gameOver() {
 
   if (!isLose) {
     d = (0, height, position.x, position.y);
-    // if collide, game over
+    // if ball collide the screen, remove heart
     if (d === 0) {
       removeHeart = true;
     }
   }
-
+  //gameover scene
   if (isLose) {
     background("black");
     fill("white");
@@ -244,15 +238,10 @@ function gameOver() {
     text("Click anywhere to start", width/2-50, height/2+30);
   }
 }
-
+//go back to game scene 
 function mouseClicked() {
   if (isLose) {
     to = 60;
     isLose = false;
   }
 }
-
-
-
-
-

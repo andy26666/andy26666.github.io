8a411039d0a8858b20db45a3e8922501a520c29a
andy26666 <7009221@spsd.sk.ca>
2024-10-29 11:01:19 -0600

    Update sketch.js

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -1,55 +1,43 @@
-// Grid Based Game Assignment
-// Kevin Lee
-// 2024/10/25
-
-// Puzzle game
-
-let cols = 3;
-let rows = 3;
-let dogImg;
-let DOGIMGWIDTH = 200;
-let DOGIMGHEIGHT = 200;
-
-let slice_x;
-let slice_y;
-
-let puzzleArr = [];
+let dogimg;
+let pieces = [];
+let cols = 3; // Number of columns
+let rows = 3; // Number of rows
 
 function preload() {
-  dogImg = loadImage("dog.png");
+  dogimg = loadImage('dog.png'); // Replace with your image filename or URL
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  cropImage();
+  createCanvas(400, 400);
+  dogimg.resize(width, height); // Resize image to fit canvas
+  let pieceWidth = dogimg.width / cols;
+  let pieceHeight = dogimg.height / rows;
+
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      let piece = dogimg.get(x * pieceWidth, y * pieceHeight, pieceWidth, pieceHeight);
+      pieces.push(piece);
+    }
+  }
+
+  // Shuffle image piece randomly
+  pieces = shuffle(pieces);
 }
 
 function draw() {
   background(220);
+  let pieceWidth = width / cols;
+  let pieceHeight = height / rows;
 
-  puzzleArr = shuffle(puzzleArr);
-
-  // Keep track of the position of the current square.
-  // We change these as we draw each square,
-  // so we know where to draw the next one.
-  let dogX = 0;
-  let dogY = 0;
-
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < cols; x++) {
-
-    }
+  for (let i = 0; i < pieces.length; i++) {
+    let x = floor(i % cols) * pieceWidth;
+    let y = floor(i / cols) * pieceHeight;
+    image(pieces[i], x, y);
   }
+
+
 }
 
-function cropImage() {
-  slice_x = DOGIMGWIDTH/cols;
-  slice_y = DOGIMGHEIGHT/rows;
-
-  for (let y = 0; y < height; y += slice_y) {
-    for (let x = 0; x < width; x += slice_x) {
-      puzzleArr.push(dogImg.get(x, y, slice_x, slice_y));
-    }
-  }
+function mouseClicked() {
 
 }
